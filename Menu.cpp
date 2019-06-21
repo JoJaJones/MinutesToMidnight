2142997412d51cd144779d70e6f56812e89774bd
JasonMRash <jasonmichaelrash@gmail.com>
2019-06-21 09:14:23 -0500

    Start setting up Menu

---
+++ Menu.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * Name: Patricia Booth, Josh Jones, Lindsey Olmstead, Jason Rash
+ * Date: 6-21-19
+ * Description: Menu class to display User Interface
+******************************************************************************/
+
+#include "Menu.hpp"
+
+Menu::Menu()
+{
+
+}
+
+void Menu::displayMenu()
+{
+
+}
+
+void getMenuChoice();
+{
+
+}
+
+bool isValidMenuChoice()
+{
+
+}
---
+++ Menu.hpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * Name: Patricia Booth, Josh Jones, Lindsey Olmstead, Jason Rash
+ * Date: 6-21-19
+ * Description: Menu class to display User Interface
+******************************************************************************/
+
+#ifndef MENU_HPP
+#define MENU_HPP
+
+class Menu()
+{
+    private:
+        char menuChoice;
+
+    public:
+        Menu();
+        void displayMenu();
+        void getMenuChoice();
+        bool isValidMenuChoice();
+};
+
+#endif
