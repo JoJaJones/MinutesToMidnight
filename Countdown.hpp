489d0a2941157a5bd662396eecfd621dcbab3944
Josh Jones <jonesjo7@oregonstate.edu>
2019-06-20 22:27:50 -0700

    Initial commit of Countdown class

---
+++ Countdown.hpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * Created by Joshua Jones on 6/20/2019.
+ * Copyright (c) 2019 
+ * All rights reserved.
+ ******************************************************************************/
+
+#ifndef BEAVERHACKSSUMMER19_COUNTDOWN_HPP
+#define BEAVERHACKSSUMMER19_COUNTDOWN_HPP
+
+
+
+class Countdown {
+
+};
+
+
+
+#endif //BEAVERHACKSSUMMER19_COUNTDOWN_HPP
