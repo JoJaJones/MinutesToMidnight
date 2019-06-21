da905ed6993d6cd97311c4aa4b9178136bf6790e
Josh Jones <jonesjo7@oregonstate.edu>
2019-06-20 23:09:27 -0700

    Created the Countdown class

--- Countdown.cpp
+++ Countdown.cpp
@@ -1,7 +1,62 @@
-/******************************************************************************
- * Created by Joshua Jones on 6/20/2019.
- * Copyright (c) 2019 
- * All rights reserved.
- ******************************************************************************/
+/**********************************************************************************
+ * Class to represent the countdown objects created by the user
+ *
+ * Will get the creation time when it is created and determine the target time
+ * based on current date and user input date
+ **********************************************************************************/
 
 #include "Countdown.hpp"
+
+/**********************************************************************************
+ * Constructor for initial creation of a countdown object
+ *
+ * Takes the name as a string and the target time as a time struct pointer
+ * and uses them to initialize the member variables
+ **********************************************************************************/
+Countdown::Countdown(std::string eventName, struct tm* targetTime) {
+    this->eventName = eventName;
+    this->targetTime = targetTime;
+
+    int temp = time(NULL); //get current time and store in temp
+    creationTime = localtime(&temp); //convert time to a tm* and store in creation time
+
+}
+
+/**********************************************************************************
+ * Constructor for recreating a countdown object from the data file
+ *
+ * Takes the name as a string, the target time as a long and the creation time
+ * as a long and uses them to initialize the member variables
+ **********************************************************************************/
+Countdown::Countdown(std::string eventName, long targetTime, long creationTime) {
+    this->eventName = eventName;
+    this->creationTime = localtime(creationTime);
+    this->targetTime = localtime(targetTime);
+}
+
+/**********************************************************************************
+ * Function to save the data stored in the countdown object to a target save file
+ **********************************************************************************/
+void Countdown::saveData(){
+    std::ofstream outFile;
+    outFile.open("data.txt", ios::app); //open file in append mode
+
+    //add data for the countdown to the end of the data file
+    outFile << mktime(creationTime) << "|" << eventName << "|" << mktime(targetTime) << std::endl;
+    //TODO(set the save code to reinitialize save file before saving all the countdowns or
+    //     to save the data only if the data does not already exist)
+}
+
+
+//accessors below this comment
+struct tm* Countdown::getTargetTime(){
+    return targetTime;
+}
+
+struct tm* Countdown::getCreationTime(){
+    return creationTime;
+}
+
+std::string Countdown::getName(){
+    return eventName;
+}
--- Countdown.hpp
+++ Countdown.hpp
@@ -1,18 +1,29 @@
-/******************************************************************************
- * Created by Joshua Jones on 6/20/2019.
- * Copyright (c) 2019 
- * All rights reserved.
- ******************************************************************************/
+/**********************************************************************************
+ * Class to represent the countdown objects created by the user
+ *
+ * Will get the creation time when it is created and determine the target time
+ * based on current date and user input date
+ **********************************************************************************/
 
 #ifndef BEAVERHACKSSUMMER19_COUNTDOWN_HPP
 #define BEAVERHACKSSUMMER19_COUNTDOWN_HPP
 
-
+#include <string>
+#include <ctime>
+#include <fstream>
 
 class Countdown {
-
+private:
+    struct tm* creationTime, targetTime;
+    std::string eventName;
+public:
+    Countdown(std::string eventName, struct tm* targetTime);
+    Countdown(std::string eventName, int targetTime, int creationTime);
+    void saveData();
+    struct tm* getTargetTime();
+    struct tm* getCreationTime();
+    std::string getName();
 };
-
 
 
 #endif //BEAVERHACKSSUMMER19_COUNTDOWN_HPP
