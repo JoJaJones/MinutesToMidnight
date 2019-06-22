847da949f2a837a8168c0af8be4c26afb1f790e9
JasonMRash <jasonmichaelrash@gmail.com>
2019-06-22 01:43:30 -0500

    Create Validation

---
+++ Validation.cpp
@@ -0,0 +1,69 @@
+/******************************************************************************
+ * Name: Patricia Booth, Josh Jones, Lindsey Olmstead, Jason Rash
+ * Date: 6-21-19
+ * Description: Validation of user input header file.
+******************************************************************************/
+
+#include "Validation.hpp"
+
+int getInt(int min, int max)
+{
+	int num = -1;
+	std::string str = "";
+	while (!isInt(str) || !isWithinRange(num, min, max) || str.empty())
+	{
+		str = "";
+		num = 0;
+	    std::getline(std::cin, str);
+		str = noSpace(str);
+		if (isInt(str))
+		{
+			for (unsigned i = 0; i < str.length(); i++)
+			{
+				num = num * 10 + (str[i] - 48);    // 0 = 48 in ascii
+			}
+		}
+		
+		if (!isInt(str) || !isWithinRange(num, min, max) || str.empty())
+		{
+			std::cout << "Please input a number between " << min
+				<< " and " << max << ".\n";
+		}
+	}
+
+	return num;
+}
+
+bool isInt(std::string str)
+{
+	for (unsigned i = 0; i < str.length(); i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return false;
+	}
+	return true;
+}
+
+bool isWithinRange(int num, int min, int max)
+{
+	if (num < min || num > max)
+		return false;
+	return true;
+}
+
+std::string noSpace(std::string str)
+{
+	std::string newStr;
+
+	for (unsigned i = 0; i < str.length(); i++)
+	{
+		if (str[i] != '\t' || str[i] != ' ')
+		{
+			newStr += str[i];
+			i++;
+		}
+
+		return newStr;
+	}
+	return newStr;
+}
